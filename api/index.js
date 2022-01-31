9df96120ff5b1a4439cc592e815be2768990fe8f
Jackman108 <arjun-pandav@yandex.ru>
2022-02-01 01:42:11 +0300

    DB crate

--- api/index.js
+++ api/index.js
@@ -2,16 +2,17 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv")
 const app = express();
+const authRoute = require("./routes/auth")
 
-dotenv.config()
+dotenv.config();
+app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL,)
-    .then(console.log("Connected to MDB"))
+mongoose
+    .connect(process.env.MONGO_URL,)
+    .then(console.log("Connected to MongoDB"))
     .catch((err) => console.log(err))
 
-
-
-
+app.use("/api/auth", authRoute);
 
 app.listen("5000", () => {
     console.log("Backend is running");
