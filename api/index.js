946a9fda742d3f9a5f372f78ae35a4e25f134680
Jackman108 <arjun-pandav@yandex.ru>
2022-02-01 15:23:43 +0300

    ERROR DB

--- api/index.js
+++ api/index.js
@@ -8,8 +8,8 @@ dotenv.config();
 app.use(express.json());
 
 mongoose
-    .connect(process.env.MONGO_URL,)
-    .then(console.log("Connected to MongoDB"))
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.log(err))
 
 app.use("/api/auth", authRoute);
